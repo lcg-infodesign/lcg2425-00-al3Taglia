c3e70ba9f2a77620ae2f3bae4fc441912a707fa6
Alessia Tagliaferri <alessia.tagliaferri@mail.polimi.it>
2024-10-25 01:29:10 +0200

    quadratini

--- sketch.js
+++ sketch.js
@@ -1,17 +1,54 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  noLoop();
+  
 }
+
+let nLines = 100;
 
 function draw() {
   // put drawing code here
-}
+  background("#b6c2cd");
+
+ 
+
+  fill('#161a1e');
+  noStroke(); 
+  let lato = 20;
+  let distanza = lato * sqrt(2);
+  
+  let nColums = (windowWidth - lato)/distanza; //lato*sqrt2
+  let nRows = (windowHeight - lato )/distanza;
+
+  
+  
+  //if qualcosa --> creo questo, else --> ruoto di QUARTER_PI
+  //qualcosa: se  100 è divisibile per 3
+
+  for(let i = 0; i < nColums; i++) { //ciclo per fare le colonne
+
+    for(let j = 0; j < nRows; j++) { //ciclo per fare le righe
+      
+      let x = i * distanza + distanza/2;
+      let y = j * distanza + distanza/2;
+      
+      push();
+      translate(x, y);
+      
+      let q = random(0, 10);
+
+      if(q < 7) {
+        rotate(QUARTER_PI);
+        
+      }
+  rectMode (CENTER);
+      
+      rect(0, 0, lato, lato); 
+      pop();
+    }
+    
+  }
+
+} 
+{}
